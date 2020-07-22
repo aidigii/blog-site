218bb9d91ee9bfcd61b98f4313e284683ee54eee
Aliah <asndeguzman@gmail.com>
2020-07-22 10:33:54 -0500

    moved index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './src/App';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import theme from './theme';
+import theme from './src/theme';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 ReactDOM.render(
